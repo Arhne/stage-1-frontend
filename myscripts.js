ed28bf9633935a7dcceeb21fec7095cbce645dcb
arh_ne <114534495+Arhne@users.noreply.github.com>
2024-07-02 13:47:02 +0100

    First file upload

---
+++ myscripts.js
@@ -0,0 +1,9 @@
+const timeUTC = () => {
+    const currentTime = new Date().toLocaleTimeString();
+    let currentDay = new Date().toLocaleString('en-us', {weekday: 'long'});
+   
+    document.querySelector('#time').textContent = currentTime;
+    document.querySelector('#day').textContent = currentDay;
+}
+setInterval(timeUTC, 1000);
+
